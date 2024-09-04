e6ff4da7711a069e75530f517c5024e383d60364
WorKa Adem <adem.bj@gmail.com>
2024-09-04 11:12:48 +0200

    Initial commit

---
+++ src/main/java/com/example/Hayaterra/HayaterraApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hayaterra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HayaterraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HayaterraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hayaterra/HayaterraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hayaterra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HayaterraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
